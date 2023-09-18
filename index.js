63ea917e8d7ae275dfc9f03227656555dd25b249
rpatil5 <rishistar05@gmail.com>
2023-09-17 21:57:03 -0500

    updated port

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ app.get("/", async (req, res) => {
 })
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 10000
 app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
